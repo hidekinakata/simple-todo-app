4b32c3ffacba388aed17831e07d32a43511fe045
William Hideki Nakata <whnakata@gmail.com>
2023-04-02 15:46:17 -0300

    Create CustomButtom.tsx

---
+++ src/components/Utils/CustomButtom.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface CustomButtomType extends React.ComponentProps<"button"> {}
+
+const CustomButtom: React.FC<CustomButtomType> = ({ children, className }) => {
+  return (
+    <>
+      <button
+        className={
+          "bg-pink-500 text-slate-50 h-fit p-2 rounded-lg ease-in-out transition-all duration-150 hover:brightness-110 active:scale-105 " +
+          className
+        }
+      >
+        {children}
+      </button>
+    </>
+  );
+};
+
+export default CustomButtom;
