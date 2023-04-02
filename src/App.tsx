d32bc33aa5004ddac7ee20bf7dae85f935c66f40
William Hideki Nakata <whnakata@gmail.com>
2023-04-02 15:43:41 -0300

    update app style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import TasksContainer from "./components/TasksContainer";
+import TaskList from "./components/Tasks/TaskList";
+import AddTaskModal from "./components/Tasks/AddTaskModal";
 
 function App() {
   return (
     <div
       className={
-        "font-poppins w-screen h-screen bg-gradient-to-br from-green-100 to-purple-50 uppercase"
+        "font-poppins w-screen h-screen bg-gradient-to-tl from-slate-100 via-fuchsia-50 to-purple-300"
       }
     >
       <Header>The simple ToDo App!</Header>
+      <TasksContainer>
+        <TaskList />
+      </TasksContainer>
+
+      <AddTaskModal />
     </div>
   );
 }
