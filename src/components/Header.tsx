bec63af97b802bb239b6d0f6b667ed9922da7402
William Hideki Nakata <whnakata@gmail.com>
2023-04-02 15:48:46 -0300

    Adjust header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import CustomInput from "./Utils/CustomInput";
+import CustomButtom from "./Utils/CustomButtom";
 
 interface HeaderType extends React.PropsWithChildren {}
 
 const Header: React.FC<HeaderType> = ({ children }) => {
+  const [title, setTitle] = useState("");
+
   return (
-    <header className="w-full py-6 flex justify-center">
-      <h1 className={"text-4xl text-slate-900 font-black"}>{children}</h1>
+    <header className="w-full py-6 flex flex-col justify-center items-center">
+      <h1
+        className={"text-lg text-slate-900 font-black sm:text-3xl md:text-4xl"}
+      >
+        {children}
+      </h1>
     </header>
   );
 };
