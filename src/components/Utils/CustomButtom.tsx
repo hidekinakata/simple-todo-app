64056b6f27878f76c3f148e0723fe4ae226c9936
William Hideki Nakata <whnakata@gmail.com>
2023-04-03 03:42:42 -0300

    inserting click handle

--- src/components/Utils/CustomButtom.tsx
+++ src/components/Utils/CustomButtom.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 interface CustomButtomType extends React.ComponentProps<"button"> {}
 
-const CustomButtom: React.FC<CustomButtomType> = ({ children, className }) => {
+const CustomButtom: React.FC<CustomButtomType> = ({
+  children,
+  className,
+  onClick,
+}) => {
   return (
     <>
       <button
+        onClick={onClick}
         className={
           "bg-pink-500 text-slate-50 h-fit p-2 rounded-lg ease-in-out transition-all duration-150 hover:brightness-110 active:scale-105 " +
           className
